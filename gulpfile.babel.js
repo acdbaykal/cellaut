58c005309b62667b3a94741971d712bcc9f00b4b
ali <abaykal@icrc.org>
2016-09-12 21:54:34 +0200

    init dev env

---
+++ gulpfile.babel.js
@@ -0,0 +1,39 @@
+import gulp from "gulp";
+import path from "path";
+import babel from "gulp-babel";
+import mocha from "gulp-mocha";
+import {log} from "gulp-util";
+
+const dest = "dist";
+const js_src=["**/*.js"];
+const js_test_src = ["**/*.test.js"];
+
+gulp.task("build:js", function(){
+  return gulp.src(js_src)
+    .pipe(babel().on("error", log))
+    .pipe(gulp.dest(dest+"/"));
+});
+
+gulp.task("build:all",  gulp.parallel("build:js"));
+
+gulp.task("watch:build", function(){
+  return gulp.watch(js_src, gulp.series("build:all"));
+});
+
+gulp.task("test:js", function(){
+  return gulp.src(js_test_src)
+            .pipe(mocha({
+              compilers:"js:babel-core/register"
+            }).on("error", log));
+});
+
+gulp.task("watch:test", gulp.series("test:js", gulp.parallel(
+  function(){
+    return gulp.watch(js_test_src, gulp.series("test:js"));
+  },
+  function(){
+    return gulp.watch(js_src, gulp.series("test:js"));
+  }
+)));
+
+gulp.task("default", gulp.parallel("watch:test", "watch:build"));
