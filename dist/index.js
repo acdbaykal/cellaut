945488c7ab5d25169d63b6a6080c2b9cedcbba0d
ali <abaykal@icrc.org>
2016-09-13 18:14:09 +0200

    corrected how index.js exports the modules

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.DOMRender = exports.CellAutArray = exports.RuleMap = undefined;
+
+var _RuleMap = require("./RuleMap/RuleMap");
+
+var _RuleMap2 = _interopRequireDefault(_RuleMap);
+
+var _CellAutArray = require("./CellAutArray/CellAutArray");
+
+var _CellAutArray2 = _interopRequireDefault(_CellAutArray);
+
+var _DOMRender = require("./Rendering/DOMRender/DOMRender");
+
+var _DOMRender2 = _interopRequireDefault(_DOMRender);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.RuleMap = _RuleMap2.default;
+exports.CellAutArray = _CellAutArray2.default;
+exports.DOMRender = _DOMRender2.default;
