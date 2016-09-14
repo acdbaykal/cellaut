4c923b269333a573aef0982cb8d001521be66f67
ali <abaykal@icrc.org>
2016-09-14 13:43:38 +0200

    1)RuleMaps can now be defined by providing the rule number 2)renamed addRule to setRule

--- dist/RuleMap/RuleMap.js
+++ dist/RuleMap/RuleMap.js
@@ -4,12 +4,14 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
-
 var _immutable = require("immutable");
 
-var RuleMap = function RuleMap(rules_param) {
-  var rules = rules_param || (0, _immutable.Map)({});
+var POSSIBLE_NEIGHBOUR_STATES = [[true, true, true], [true, true, false], [true, false, true], [true, false, false], [false, true, true], [false, true, false], [false, false, true], [false, false, false]];
+
+var POWERS_OF_TWO = [128, 64, 32, 16, 8, 4, 2, 1];
+
+var RuleMap = function RuleMap() {
+  var _arguments = arguments;
 
   function _valuesToKey(left_top, center_top, right_top) {
     var left_top_key = left_top ? "1" : "0";
@@ -19,24 +21,33 @@ var RuleMap = function RuleMap(rules_param) {
     return left_top_key + center_top_key + right_top_key;
   }
 
-  function addRule() {
-    var _arguments = arguments;
+  //set initial rules
+  var rules = function () {
+    if (_arguments.length === 0) {
+      return (0, _immutable.Map)({});
+    } else {
+      var param = _arguments[0];
+      if (param instanceof _immutable.Map) {
+        return param;
+      } else if (typeof param === "number") {
+        var rule_number = Math.round(param) % 256; //cutoff anything larger than 255
+        var result = {};
+        var remain = rule_number;
+        for (var i = 0; i < 8; i++) {
+          var p = POWERS_OF_TWO[i];
+          var int_div = Math.floor(remain / p); //0 or 1
+          remain -= p * int_div;
 
-    var _apply = function () {
-      if (_arguments.length === 4) {
-        return _arguments;
+          var neighbour_state = POSSIBLE_NEIGHBOUR_STATES[i];
+          var key = _valuesToKey.apply(undefined, neighbour_state);
+          result[key] = Boolean(int_div);
+        }
+        return (0, _immutable.Map)(result);
       }
-      return [_arguments[0], _arguments[1], undefined, _arguments[2]];
-    }.apply(undefined, arguments);
+    }
+  }.apply(undefined, arguments);
 
-    var _apply2 = _slicedToArray(_apply, 4);
-
-    var left_top = _apply2[0];
-    var center_top = _apply2[1];
-    var right_top = _apply2[2];
-    var self = _apply2[3];
-
-
+  function setRule(left_top, center_top, right_top, self) {
     var key = _valuesToKey(left_top, center_top, right_top);
     var new_rules = rules.set(key, self);
     return RuleMap(new_rules);
@@ -51,7 +62,7 @@ var RuleMap = function RuleMap(rules_param) {
     return JSON.stringify(rules.toObject());
   }
 
-  return { addRule: addRule, getValue: getValue, toString: toString };
+  return { setRule: setRule, getValue: getValue, toString: toString };
 };
 
 exports.default = RuleMap;
--- dist/RuleMap/test/RuleMap.test.js
+++ dist/RuleMap/test/RuleMap.test.js
@@ -18,9 +18,45 @@ describe("RuleMap", function () {
 
       (0, _chai.expect)(construct).to.not.throw();
     });
+
+    it("should accept a rule number (0 - 255)", function () {
+      var map = (0, _RuleMap2.default)(109);
+      (0, _chai.expect)(map.getValue(true, true, true)).to.be.equal(false);
+
+      (0, _chai.expect)(map.getValue(true, true, false)).to.be.equal(true);
+
+      (0, _chai.expect)(map.getValue(true, false, true)).to.be.equal(true);
+
+      (0, _chai.expect)(map.getValue(true, false, false)).to.be.equal(false);
+
+      (0, _chai.expect)(map.getValue(false, true, true)).to.be.equal(true);
+
+      (0, _chai.expect)(map.getValue(false, true, false)).to.be.equal(true);
+
+      (0, _chai.expect)(map.getValue(false, false, true)).to.be.equal(false);
+
+      (0, _chai.expect)(map.getValue(false, false, false)).to.be.equal(true);
+
+      map = (0, _RuleMap2.default)(254);
+      (0, _chai.expect)(map.getValue(true, true, true)).to.be.equal(true);
+
+      (0, _chai.expect)(map.getValue(true, true, false)).to.be.equal(true);
+
+      (0, _chai.expect)(map.getValue(true, false, true)).to.be.equal(true);
+
+      (0, _chai.expect)(map.getValue(true, false, false)).to.be.equal(true);
+
+      (0, _chai.expect)(map.getValue(false, true, true)).to.be.equal(true);
+
+      (0, _chai.expect)(map.getValue(false, true, false)).to.be.equal(true);
+
+      (0, _chai.expect)(map.getValue(false, false, true)).to.be.equal(true);
+
+      (0, _chai.expect)(map.getValue(false, false, false)).to.be.equal(false);
+    });
   });
 
-  describe("addRule", function () {
+  describe("setRule", function () {
     var rule_tree = void 0;
 
     beforeEach(function () {
@@ -28,22 +64,13 @@ describe("RuleMap", function () {
     });
 
     it("should have the function", function () {
-      (0, _chai.expect)(rule_tree).to.include.keys("addRule");
+      (0, _chai.expect)(rule_tree).to.include.keys("setRule");
     });
 
     it("should allow four boolean parameters and return a new RuleMap", function () {
       var result = void 0;
       (0, _chai.expect)(function () {
-        result = rule_tree.addRule(true, true, true, true);
-      }).not.to.throw();
-
-      (0, _chai.expect)(result).not.to.equal(rule_tree);
-    });
-
-    it("should allow three boolean parameters and return a new RuleMap", function () {
-      var result = void 0;
-      (0, _chai.expect)(function () {
-        result = rule_tree.addRule(true, true, true);
+        result = rule_tree.setRule(true, true, true, true);
       }).not.to.throw();
 
       (0, _chai.expect)(result).not.to.equal(rule_tree);
@@ -51,19 +78,13 @@ describe("RuleMap", function () {
   });
 
   describe("getValue", function () {
-    var rule_tree = void 0;
-
-    beforeEach(function () {
-      rule_tree = (0, _RuleMap2.default)().addRule(true, true, true, true).addRule(true, true, false, false).addRule(true, true, true).addRule(true, false, false);
-    });
+    var rule_tree = (0, _RuleMap2.default)().setRule(true, true, true, true).setRule(true, true, false, false);
 
     it("should have the function", function () {
       (0, _chai.expect)(rule_tree).to.include.keys("getValue");
     });
 
-    it("should allow four two to three boolean parameters and return the value for this rule", function () {
-      (0, _chai.expect)(rule_tree.getValue(true, true)).to.equal(true);
-      (0, _chai.expect)(rule_tree.getValue(true, false)).to.equal(false);
+    it("should take three boolean parameters and return the value for this rule", function () {
       (0, _chai.expect)(rule_tree.getValue(true, true, true)).to.equal(true);
       (0, _chai.expect)(rule_tree.getValue(true, true, false)).to.equal(false);
     });
